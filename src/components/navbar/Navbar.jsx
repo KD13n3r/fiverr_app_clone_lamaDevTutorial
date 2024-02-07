41d69f74b285fdfeb600e69170eef8fb5e800e23
KD13n3r <kevin.diener@younglings.africa>
2024-02-07 13:58:37 +0200

    scss for the menu/options

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
           {!currentUser && <button>Join</button>}
           {currentUser && (
             <div className="user">
-              <img src="" alt="" />
+              <img src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
               <span>{currentUser.username}</span>
               <div className="options">
                 {currentUser?.isSeller && (
