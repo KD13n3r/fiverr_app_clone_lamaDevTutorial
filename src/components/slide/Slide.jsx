3716e6d4305d23fecb07c942f2c69bac5b99851b
KD13n3r <kevin.diener@younglings.africa>
2024-02-10 13:34:06 +0200

    completed infinite slide component

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -3,13 +3,12 @@ import React from "react";
 import "./Slide.scss";
 import { Slider } from "infinite-react-carousel";
 
-
-const Slide = ({children,slidesToShow,arrowsScroll}) => {
+const Slide = ({ children, slidesToShow, arrowsScroll }) => {
   return (
     <div className="slide">
       <div className="container">
         <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
-         {children}
+          {children}
         </Slider>
       </div>
     </div>
