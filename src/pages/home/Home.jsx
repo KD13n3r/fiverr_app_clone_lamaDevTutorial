6ac938e780c852c43bbfcc3c29c6a01d3a656e40
KD13n3r <kevin.diener@younglings.africa>
2024-02-13 13:25:57 +0200

    featured section of home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,26 +1,49 @@
-import React from 'react'
-import "./Home.scss"
-import Featured from '../../components/featured/Featured'
-import TrustedBy from '../../components/trustedBy/TrustedBy'
-import Slide from '../../components/slide/Slide'
-import {cards} from "../../data"
-import CatCard from '../../components/catCard/CatCard'
+import React from "react";
+import "./Home.scss";
+import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/slide/Slide";
+import { cards } from "../../data";
+import CatCard from "../../components/catCard/CatCard";
 
 const Home = () => {
   return (
-    <div className='home'>
-      <Featured/>
-      <TrustedBy/>
+    <div className="home">
+      <Featured />
+      <TrustedBy />
       <Slide slidesToShow={5} arrowsScroll={5}>
-        {cards.map(card=>(
-          <CatCard key={card.id} item={card}/>
+        {cards.map((card) => (
+          <CatCard key={card.id} item={card} />
         ))}
       </Slide>
-      <div className='features'>
-        
+      <div className="features">
+        <div className="container">
+          <div className="item">
+            <h1>A whole world of freelance talent at your fignertips</h1>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No horly rates,
+              just project-Based pricing.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find high-quality services at every price point. No horly rates,
+              just project-Based pricing.
+            </p>
+          </div>
+          <div className="item">
+            <video src="./img/video.mp4" controls></video>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
