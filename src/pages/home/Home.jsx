ff34dc65bf8d1fe427698a19fa07158008f32b6c
KD13n3r <kevin.diener@younglings.africa>
2024-02-14 14:08:47 +0200

    ProjectCard html

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.scss";
 import Featured from "../../components/featured/Featured";
 import TrustedBy from "../../components/trustedBy/TrustedBy";
 import Slide from "../../components/slide/Slide";
-import { cards } from "../../data";
+import { cards, projects } from "../../data";
 import CatCard from "../../components/catCard/CatCard";
 
 const Home = () => {
@@ -58,7 +58,6 @@ const Home = () => {
           </div>
         </div>
       </div>
-      {/*  */}
       <div className="features dark">
         <div className="container">
           <div className="item">
@@ -69,16 +68,16 @@ const Home = () => {
               benefits,dedicated to business
             </p>
             <div className="title">
-              <img src="./img/check.png" alt=""/>
+              <img src="./img/check.png" alt="" />
               Connect to freelancers with proven business experience
             </div>
             <div className="title">
-              <img src="./img/check.png" alt=""/>
+              <img src="./img/check.png" alt="" />
               Get matched with the perfect talent by a customer success manager
             </div>
 
             <div className="title">
-              <img src="./img/check.png" alt=""/>
+              <img src="./img/check.png" alt="" />
               Manage teamwork and boost productivity with one powerful workspace
             </div>
             <button>Explore Fiverr Business</button>
@@ -91,6 +90,11 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {projects.map((card) => (
+          <CatCard key={card.id} item={card} />
+        ))}
+      </Slide>
     </div>
   );
 };
