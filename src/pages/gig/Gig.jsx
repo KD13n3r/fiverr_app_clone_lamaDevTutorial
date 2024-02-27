ae95899928bc4b83f759aedbaabb117ac8e84ed0
KD13n3r <kevin.diener@younglings.africa>
2024-02-27 14:40:16 +0200

    left side completed of gig

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -1,30 +1,35 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from 'react'
-import "./Gig.scss"
-import { Slider } from 'infinite-react-carousel'
+import React from "react";
+import "./Gig.scss";
+import { Slider } from "infinite-react-carousel";
 
 const Gig = () => {
   return (
-    <div className='gig'>
-      <div className='container'>
-        <div className='left'>
-          <span className='breadCrumbs'>FIVERR {">"} GRAPHICS & DESIGN {">"}</span>
+    <div className="gig">
+      <div className="container">
+        <div className="left">
+          <span className="breadCrumbs">
+            FIVERR {">"} GRAPHICS & DESIGN {">"}
+          </span>
           <h1>I will creat ai generated art for you</h1>
 
-          <div className='user'>
-            <img src='' alt=''/>
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
             <span>John Doe</span>
-            <div className='stars'>
-              <img src='/img/star.png' alt=''/>
-              <img src='/img/star.png' alt=''/>
-              <img src='/img/star.png' alt=''/>
-              <img src='/img/star.png' alt=''/>
-              <img src='/img/star.png' alt=''/>
+            <div className="stars">
+              <img src="/img/star.png" alt="" />
+              <img src="/img/star.png" alt="" />
+              <img src="/img/star.png" alt="" />
+              <img src="/img/star.png" alt="" />
+              <img src="/img/star.png" alt="" />
               <span>5</span>
             </div>
           </div>
           <Slider slideToShow={1} arrowsScroll={1}>
-          <img
+            <img
               src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
               alt=""
             />
@@ -53,14 +58,173 @@ const Gig = () => {
             found nowhere else. If you have any questions you're more than
             welcome to send me a message.
           </p>
-          <div className='seller'>
+          <div className="seller">
             <h2>About the Seller</h2>
+            <div className="user">
+              <img
+                src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <div className="info">
+                <span>John Doe</span>
+                <div className="stars">
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <span>5</span>
+                </div>
+                <button>Contact Me</button>
+              </div>
+            </div>
+            <div className="box">
+              <div className="items">
+                <div className="item">
+                  <span className="title">From</span>
+                  <span className="desc">USA</span>
+                </div>
+                <div className="item">
+                  <span className="title">Member since</span>
+                  <span className="desc">Aug 2022</span>
+                </div>
+                <div className="item">
+                  <span className="title">Avg. response time</span>
+                  <span className="desc">4 hours</span>
+                </div>
+                <div className="item">
+                  <span className="title">Last delivery</span>
+                  <span className="desc">1 day</span>
+                </div>
+                <div className="item">
+                  <span className="title">Languages</span>
+                  <span className="desc">English</span>
+                </div>
+              </div>
+              <hr/>
+              <p>
+                My name is Ciaran, I enjoy creating AI generated art in my spare
+                time. I have a lot of experience using the AI program and that
+                means I know what to prompt the AI with to get a great and
+                incredibly detailed result.
+              </p>
+            </div>
+          </div>
+          <div className="reviews">
+            <h2>Reviews</h2>
+            <div className="item">
+              <div className="user">
+                <img src="https://images.pexels.com/photos/839586/pexels-photo-839586.jpeg?auto=compress&cs=tinysrgb&w=1600" alt=""/>
+                <div className="info">
+                  <span>John Doe</span>
+                  <div className="country">
+                    <img src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png" alt=""/>
+                    <span>United States</span>
+                  </div>
+                </div>
+              </div>
+                <div className="stars">
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <span>5</span>
+                </div>
+                <p>
+                I just want to say that art_with_ai was the first, and after
+                this, the only artist Ill be using on Fiverr. Communication was
+                amazing, each and every day he sent me images that I was free to
+                request changes to. They listened, understood, and delivered
+                above and beyond my expectations. I absolutely recommend this
+                gig, and know already that Ill be using it again very very soon
+              </p>
+              <div className="helpful">
+                <span>Helpful?</span>
+                <img src="/img/like.png" alt=""/>
+                <span>Yes</span>
+                <img src="/img/dislike.png" alt=""/>
+                <span>No</span>
+              </div>
+            </div>
+            <hr/>
+            <div className="item">
+              <div className="user">
+                <img src="https://images.pexels.com/photos/839586/pexels-photo-839586.jpeg?auto=compress&cs=tinysrgb&w=1600" alt=""/>
+                <div className="info">
+                  <span>John Doe</span>
+                  <div className="country">
+                    <img src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png" alt=""/>
+                    <span>United States</span>
+                  </div>
+                </div>
+              </div>
+                <div className="stars">
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <span>5</span>
+                </div>
+                <p>
+                I just want to say that art_with_ai was the first, and after
+                this, the only artist Ill be using on Fiverr. Communication was
+                amazing, each and every day he sent me images that I was free to
+                request changes to. They listened, understood, and delivered
+                above and beyond my expectations. I absolutely recommend this
+                gig, and know already that Ill be using it again very very soon
+              </p>
+              <div className="helpful">
+                <span>Helpful?</span>
+                <img src="/img/like.png" alt=""/>
+                <span>Yes</span>
+                <img src="/img/dislike.png" alt=""/>
+                <span>No</span>
+              </div>
+            </div>
+            <hr/>
+            <div className="item">
+              <div className="user">
+                <img src="https://images.pexels.com/photos/839586/pexels-photo-839586.jpeg?auto=compress&cs=tinysrgb&w=1600" alt=""/>
+                <div className="info">
+                  <span>John Doe</span>
+                  <div className="country">
+                    <img src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png" alt=""/>
+                    <span>United States</span>
+                  </div>
+                </div>
+              </div>
+                <div className="stars">
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <span>5</span>
+                </div>
+                <p>
+                I just want to say that art_with_ai was the first, and after
+                this, the only artist Ill be using on Fiverr. Communication was
+                amazing, each and every day he sent me images that I was free to
+                request changes to. They listened, understood, and delivered
+                above and beyond my expectations. I absolutely recommend this
+                gig, and know already that Ill be using it again very very soon
+              </p>
+              <div className="helpful">
+                <span>Helpful?</span>
+                <img src="/img/like.png" alt=""/>
+                <span>Yes</span>
+                <img src="/img/dislike.png" alt=""/>
+                <span>No</span>
+              </div>
+            </div>
           </div>
         </div>
-        <div className='right'></div>
+        <div className="right"></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Gig
+export default Gig;
