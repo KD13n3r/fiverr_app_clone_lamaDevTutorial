fe4584957f09ca0fc8c2a18ba01ee9827fc23b62
KD13n3r <kevin.diener@younglings.africa>
2024-02-29 13:17:46 +0200

    added styles and html to Orders.jsx

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,10 +1,115 @@
-import React from 'react'
-import "./Orders.scss"
+import React from "react";
+import "./Orders.scss";
 
 const Orders = () => {
+  const currentUser = {
+    id: 1,
+    username: "Kevin Diener",
+    isSeller: true,
+  };
   return (
-    <div className='orders'>Orders</div>
-  )
-}
+    <div className="orders">
+      <div className="container">
+        <div className="title">
+          <h1>Orders</h1>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>{currentUser?.isSeller ? "Buyer" : "Seller"}</th>
+            <th>Contact</th>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <button>
+                <img className="delete" src="/img/message.png" alt="" />
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <button>
+                <img className="delete" src="/img/message.png" alt="" />
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <button>
+                <img className="delete" src="/img/message.png" alt="" />
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <button>
+                <img className="delete" src="/img/message.png" alt="" />
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img
+                className="img"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Gig1</td>
+            <td>88</td>
+            <td>123</td>
+            <td>
+              <button>
+                <img className="delete" src="/img/message.png" alt="" />
+              </button>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
 
-export default Orders
+export default Orders;
