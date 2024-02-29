60404ccba38a44711f9f1a54d0f3f1e49d4a7aa9
KD13n3r <kevin.diener@younglings.africa>
2024-02-29 14:16:58 +0200

    message html added

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import "./Message.scss"
+import React from "react";
+import "./Message.scss";
+import { Link } from "react-router-dom";
 
 const Message = () => {
   return (
-    <div className='message'>Message</div>
-  )
-}
+    <div className="message">
+      <div className="container">
+        <span className="breadcrumbs">
+          <Link to="/messages">MESSAGES </Link> {">"} JOHN DOE {">"}
+        </span>
+        <div className="messages">
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. 
+            </p>
+          </div>
+        </div>
+        <div className="write">
+          <textarea
+            name=""
+            id=""
+            placeholder="write a message"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button>Send</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
