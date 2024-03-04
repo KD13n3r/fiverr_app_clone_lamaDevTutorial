aeffca488a6bf4fe86cd66c42e590a3b0e352bf2
KD13n3r <kevin.diener@younglings.africa>
2024-03-04 14:56:08 +0200

    left add section html

--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import "./Add.scss"
+import React from "react";
+import "./Add.scss";
 
 const Add = () => {
   return (
-    <div className='add'>Add</div>
-  )
-}
+    <div className="add">
+      <div className="container">
+        <h1>Add New Gig</h1>
+        <div className="sections">
+          <div className="left">
+            <label htmlFor="">Title</label>
+            <input
+              type="text"
+              placeholder="e.g. I will do something I'm really good at"
+              className=""
+            />
+            <label htmlFor="">Category</label>
+            <select name="cats" id="cats">
+              <option value="design">Design</option>
+              <option value="web">Web Development</option>
+              <option value="animation">Animation</option>
+              <option value="music">Music</option>
+            </select>
+            <label htmlFor="">Cover Image</label>
+            <input type="file" />
+            <label htmlFor="">Upload Images</label>
+            <lable type="file" multiple />
+            <label htmlFor="">Description</label>
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="16"
+              placeholder="Brief description to introduce your service to customers"
+            ></textarea>
+            <button>Create</button>
+          </div>
+          <div className="right"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Add
+export default Add;
